e301078824242a25947e44ad239dbbbabc176fc6
Heitor Roberto <rheitor127@gmail.com>
2026-08-06 01:17:53 -0300

    Realizadas 2 implementações
    Foram criadas 2 funcionalidades:
    
    - Remover estoque manualmente: Agora é possível, via terminal, realizar uma baixa no estoque manualmente.
    
    - Alterar nome do produto: Agora é possível também, realizar a alteração do nome de um produto.

--- interface/modules/inventory/edit_menu.py
+++ interface/modules/inventory/edit_menu.py
@@ -1,7 +1,11 @@
 from interface.core.terminal import Terminal
-
+from services.inventory.product_service import ProductService
 
 class EditMenu:
+    
+    def __init__(self):
+        self._product_service = ProductService()
+    
     def run(self) -> None:
         while True:
             Terminal.header("Inventário", "Edições de produto")
@@ -20,8 +24,7 @@ class EditMenu:
             option = Terminal.ask_option("Digite a opção desejada", range(1, 7))
 
             if option == 1:
-                Terminal.error('Função em desenvolvimento')
-                Terminal.pause()
+                self.change_product_name()
             elif option == 2:
                 Terminal.error('Função em desenvolvimento')
                 Terminal.pause()
@@ -36,3 +39,91 @@ class EditMenu:
                 Terminal.pause()
             elif option == 6:
                 break
+
+    def change_product_name(self):
+        
+            Terminal.clear()
+            products = self._product_service.list_products()
+            rows = []
+            chose_product = None
+            chose_product_id = None
+            new_name = None
+            exib_name = None
+    
+            if products:
+                while True:
+                    products = self._product_service.list_products()
+                    rows = []
+                    Terminal.header("Área de Edição", "Alterar Nome de Produto")
+    
+                    for c, product in enumerate(products, start=1):
+                        status = product.status.value if hasattr(product.status, "value") else product.status
+                        rows.append(
+                            [
+                                c,
+                                product.name.title(),
+                                product.category.name.capitalize(),
+                                product.stock_quantity,
+                                Terminal.money(product.sale_value),
+                                status,
+                            ]
+                        )
+    
+                    Terminal.header("Produtos cadastrados", "Inventário")
+                    Terminal.table(
+                        ["ID", "Produto", "Categoria", "Estoque", "Venda", "Status"],
+                        rows,
+                    )
+    
+                    Terminal.separator()
+    
+                    if chose_product is not None:
+                        Terminal.field(1, 'Produto', chose_product.name.title())
+                    else:
+                        Terminal.field(1, 'Produto', 'Não selecionado')
+    
+                    if new_name is not None:
+                        Terminal.field(2, 'Novo Nome', exib_name)
+                    else:
+                        Terminal.field(2, 'Novo Nome', 'Não selecionado')
+                    Terminal.option(3, 'Confirmar')
+                    Terminal.option(4, 'Cancelar')
+    
+                    Terminal.separator()
+    
+                    user_choice = Terminal.ask_option("Escolha a opção desejada", range(1, 5))
+    
+                    if user_choice == 1:
+                        chose_product_id = Terminal.ask_int('Selecione o Produto')
+                        chose_product = products[chose_product_id - 1]
+                        Terminal.success('Produto selecionado com sucesso')
+                        Terminal.clear()
+    
+                    elif user_choice == 2:
+                        if chose_product is not None:
+                            new_name = Terminal.ask('Digite o novo nome do produto')
+                            exib_name = new_name
+                        else:
+                            Terminal.error('Nenhum produto selecionado')
+                            print('')
+                            Terminal.pause()
+    
+                    elif user_choice == 3:
+                        if chose_product and chose_product.id is not None and new_name is not None:
+                            chose_product.change_name(new_name)
+                            self._product_service.save_information(chose_product, chose_product.id)
+                            Terminal.success('Nome alterado com sucesso.')
+                            print('')
+                            Terminal.pause()
+                            break
+                        else:
+                            Terminal.error('Necessário selecionar um produto ou adicionar uma quantidade maior que 0.')
+                            Terminal.pause()
+    
+                    elif user_choice == 4:
+                        break
+            else:
+                Terminal.header("Inventário", "Entrada de Produto")
+                print('Nenhum produto cadastrado.')
+                print()
+                Terminal.pause()
